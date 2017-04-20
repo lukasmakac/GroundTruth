1a7ec6e4f8f54b144039f3f61713ab5185d6eda0
Lukáš Maruniak <lukas.maruniak@morosystems.cz>
2017-04-20 12:47:34 +0200

    Spot marking implementation

--- modules/ImageWidget.py
+++ modules/ImageWidget.py
@@ -1,24 +1,28 @@
-from PIL import Image
-from PIL.ImageQt import ImageQt
-from PyQt5.QtCore import QRect, QCoreApplication, QRectF, Qt, QLine, QPoint, QLineF
-from PyQt5.QtGui import QPixmap, QPainter, QPen, QImage
-from PyQt5.QtWidgets import QWidget, QGraphicsScene, QGraphicsView, QPushButton, QVBoxLayout
-import time
+from PyQt5.QtCore import QRect, QRectF, Qt, QPoint, QLineF, QSizeF
+from PyQt5.QtGui import QPixmap, QPen, QImage, QPicture, QPainter
+from PyQt5.QtWidgets import QWidget, QGraphicsScene, QGraphicsView, QVBoxLayout, QGraphicsPixmapItem
 import cv2
+import math
 
 __author__ = 'Lukas'
 class ImageWidget(QWidget):
 
+    viewportWidth = 800
+    viewportHeight = 600
+
     cvImage = None
     img = None
     pixMap = None
+
+    gridSize = 10
+    points = []
 
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         self.scene = QGraphicsScene()
         self.view = QGraphicsView()
         self.view.setScene(self.scene)
-        self.view.setGeometry(QRect(0, 0, 800, 600))
+        self.view.setGeometry(QRect(0, 0, self.viewportWidth, self.viewportHeight))
 
         layout = QVBoxLayout()
         layout.addWidget(self.view)
@@ -34,28 +38,95 @@ class ImageWidget(QWidget):
 
         self.scene.update()
 
-    def paintEvent(self, QPaintEvent):
-        grid_size = 10
+    def paintEvent(self, event):
         if(self.img is not None):
             self.scene.clear()
             width = self.img.width()
             height = self.img.height()
-
             pixMap = QPixmap.fromImage(self.img)
+
+            #draw grid
+            #self.drawGrid(pixMap, width, height, self.gridSize)
+
+            #mark spots
+            if(self.points):
+                for point in self.points:
+                    self.markSpot(pixMap, point)
+
+            self.scene.mousePressEvent = self.onSceneClick
             self.scene.addPixmap(pixMap)
 
-            # self.view.fitInView(QRectF(0, 0, width, height), Qt.KeepAspectRatio)
-            self.view.fitInView(QRectF(0, 0, width, height), Qt.KeepAspectRatioByExpanding)
+            #self.view.fitInView(QRectF(0, 0, width, height), Qt.KeepAspectRatioByExpanding)
 
             #draw vertical lines
-            for i in range(0, width, grid_size):
+            for i in range(0, width, self.gridSize):
                 self.scene.addLine(QLineF(QPoint(i, 0), QPoint(i, height)), QPen(Qt.black))
 
             #draw horizontal lines
-            for i in range(0, height, grid_size):
+            for i in range(0, height, self.gridSize):
                 self.scene.addLine(QLineF(QPoint(0, i), QPoint(width, i)), QPen(Qt.black))
 
+            self.view.fitInView(QRectF(0, 0, width, height), Qt.KeepAspectRatio)
+
             self.scene.update()
+
+
+    # def mousePressEvent(self, QMouseEvent):
+    #     #pos of view
+    #     point = QMouseEvent.pos()
+    #
+    #     aspectRatioX = self.img.width()/self.view.width()
+    #     aspectRatioY = self.img.height()/self.view.height()
+    #
+    #     #compute coords for img (viewport can be smaller/higher than original image)
+    #     x = math.floor(point.x()*aspectRatioX / self.gridSize)*self.gridSize
+    #     y = math.floor(point.y()*aspectRatioY / self.gridSize)*self.gridSize
+    #
+    #     self.points.append(QPoint(x, y))
+    #
+    #     print("x: {}, y: {}".format(x, y))
+    #
+    #     self.view.repaint()
+
+    def onSceneClick(self, QGraphicsSceneMouseEvent):
+        #pos of view
+        point = QGraphicsSceneMouseEvent.scenePos()
+
+        #compute coords for img (viewport can be smaller/higher than original image)
+        x = math.floor(point.x()/ self.gridSize)*self.gridSize
+        y = math.floor(point.y()/ self.gridSize)*self.gridSize
+
+        self.points.append(QPoint(x, y))
+
+        print("x: {}, y: {}".format(x, y))
+
+        self.view.repaint()
+
+    def markSpot(self, paintDevice, point):
+        rect = QRectF(point.x(), point.y(), self.gridSize, self.gridSize)
+
+        painter = QPainter()
+        painter.begin(paintDevice)
+        painter.setOpacity(.5)
+        painter.fillRect(rect, Qt.white)
+        painter.end()
+
+        self.scene.addRect(rect, Qt.green)
+
+    def drawGrid(self, paintDevice, width, height, gridSize):
+        painter = QPainter()
+        painter.begin(paintDevice)
+        painter.setPen(QPen(Qt.black))
+
+        #draw vertical lines
+        for i in range(0, width, gridSize):
+            painter.drawLine(QLineF(QPoint(i, 0), QPoint(i, height)))
+
+        #draw horizontal lines
+        for i in range(0, height, gridSize):
+            painter.drawLine(QLineF(QPoint(0, i), QPoint(width, i)))
+
+        painter.end()
 
 
 
